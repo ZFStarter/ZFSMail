93338d414907b98205a4d7ed7708aa35dcd189b4
Alexander Khaylo <alexander.khaylo@nixsolutions.com>
2014-04-07 15:47:12 +0300

    Renamed dependency

--- src/ZFStarterMail/Model/Gateway/GatewayAbstractFactory.php
+++ src/ZFStarterMail/Model/Gateway/GatewayAbstractFactory.php
@@ -3,14 +3,14 @@
 namespace ZFStarterMail\Model\Gateway;
 
 use DomainModel\Feature\FilterColumnsFeature;
-use DomainModel\Gateway\DomainModelTableGatewayAbstractFactory;
-use DomainModel\Gateway\DomainModelTableGatewayAbstractFactory\Options;
+use DomainModel\Gateway\DomainObjectTableGatewayAbstractFactory;
+use DomainModel\Gateway\DomainObjectTableGatewayAbstractFactory\Options;
 
 /**
  * Class GatewayAbstractFactory
  * @package ZFStarterMail\Model\Gateway
  */
-class GatewayAbstractFactory extends DomainModelTableGatewayAbstractFactory
+class GatewayAbstractFactory extends DomainObjectTableGatewayAbstractFactory
 {
     public function __construct()
     {
