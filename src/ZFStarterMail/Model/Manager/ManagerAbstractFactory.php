7d7373312cead184874005abac4ae03db3ec02e7
farior2109 <srgspbabko@gmail.com>
2015-02-27 19:27:20 +0200

    ManagerAbstractFactory now implemnents AbstractFactory interface

--- src/ZFStarterMail/Model/Manager/ManagerAbstractFactory.php
+++ src/ZFStarterMail/Model/Manager/ManagerAbstractFactory.php
@@ -1,15 +1,26 @@
 <?php
 namespace ZFStarterMail\Model\Manager;
 
-use Common\ServiceManager\AbstractServiceAbstractFactory;
+use Zend\ServiceManager\AbstractFactoryInterface;
+use Zend\ServiceManager\ServiceLocatorInterface;
 
 /**
  * Class ManagerAbstractFactory
  * @package ZFStarterMail\Model\Manager
  */
-class ManagerAbstractFactory extends AbstractServiceAbstractFactory
+class ManagerAbstractFactory implements AbstractFactoryInterface
 {
     protected $provides = array(
         'MailTemplatesManager' => 'ZFStarterMail\Model\Manager\MailTemplatesManager'
     );
+
+    public function canCreateServiceWithName(ServiceLocatorInterface $serviceLocator, $name, $requestedName)
+    {
+        return array_key_exists($requestedName, $this->provides);
+    }
+
+    public function createServiceWithName(ServiceLocatorInterface $serviceLocator, $name, $requestedName)
+    {
+        return new $this->provides[$requestedName]();
+    }
 }
